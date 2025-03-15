cad4c28d749626d4ab3fb2d8204906b8903e3b57
Oli Davey <olidavey88@gmail.com>
2025-03-16 03:08:14 +1100

    bug fixes

--- sh.c
+++ sh.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
 /*
 shell program using c
 */
@@ -58,6 +61,152 @@ struct backcmd
     int type;
     struct cmd *cmd;
 };
+
+int fork1(void);
+void panic(char*);
+struct cmd *parsecmd(char*);
+
+void runcmd(struct cmd *cmd){ // dosnt return jsut finishes by terminating a process
+    int p[2];
+    struct backcmd *bcmd;
+    struct execcmd *ecmd;
+    struct listcmd *lcmd;
+    struct pipecmd *pcmd;
+    struct redircmd *rcmd;
+
+    if (cmd == 0){
+        exit(1);
+    }
+
+    switch (cmd -> type)
+    {
+    default:
+        panic("runcmd");
+
+    case EXEC:
+        ecmd = (struct execcmd*)cmd;
+        // checking to see there is at least one argument
+        if (ecmd->argv[0] == 0){ // argv[0] points to null terminated string of file name of program to execute
+            exit(1);
+        }
+        exec(ecmd->argv[0], ecmd->argv); // sys call exec(filename of program to be run, pointer to array)
+        printf(2, "exec %s failed\n", ecmd->argv[0]);
+        break;
+
+    case REDIR:
+        rcmd = (struct redircmd*)cmd;
+        close(rcmd->fd);
+        if (open(rcmd->file, rcmd->mode) < 0){
+            printf(2, "open %s failed\n", rcmd->file);
+            exit(1);
+        }
+        runcmd(rcmd->cmd);
+        break;
+
+    case PIPE:
+        pcmd = (struct pipecmd*)cmd;
+        if (pipe(p) < 0){ // system call pipe
+            panic("pipe");
+        }
+        // redirect pgm1 output to pgm2 input (pgm1 | pgm2)
+        if (fork1() == 0){ // creates first child process
+            close(1); //close std out
+            dup(p[1]); // creates copy of fd
+            close(p[0]); // close fd #
+            close(p[1]); // close next fd
+            runcmd(pcmd->left);
+        }
+        if (fork1() == 0){ // creates second child process
+            close(0); // close std in
+            dup(p[0]); // creats copy of file descriptor fd
+            close(p[0]);
+            close(p[1]);
+            runcmd(pcmd->right);
+        }
+        close(p[0]); // close fd 1
+        close(p[1]); // close fd 2
+        wait(0); // wait for first child to terminate
+        wait(0); // waits for other child to terminate
+        break;
+
+    case LIST:
+        lcmd = (struct listcmd*)cmd;
+        if (fork1() == 0){
+            runcmd(lcmd -> left);
+        }
+        wait(0);
+        runcmd(lcmd->right);
+        break;
+
+    case BACK:
+        bcmd = (struct backcmd*)cmd;
+        if (fork1() == 0){
+            runcmd(bcmd->cmd);
+        }
+        break;
+    }
+    exit(0);
+}
+
+
+int getcmd(char *buf, int nbuf){
+    printf(2, "$ ");
+    memset(buf, 0, nbuf);
+    gets(buf);
+    gets(nbuf);
+    if (buf[0] == 0){
+        return -1;
+    }
+    return 0;
+}
+
+
+int main(void){
+    static char buf[100]; // num of chars the buffer will allow (how big of a cmd we can pass)
+    int fd; // file descriptor 
+
+    while ((fd = open("console", O_RDWR)) >= 0) // open for reading and writing
+    {
+        if (fd >= 3){
+            close(fd);
+            break;
+        }
+    }
+
+    // read then run cmd
+    while (getcmd(buf, sizeof(buf)) >= 0)
+    {
+        if (buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' ') // checking for cd dir
+        {
+            buf[strlen(buf)-1] = 0; // buf[length of string -1] set to 0
+            if (chdir(buf+3) < 3){ // inokes chdir sys call and points to file name
+                printf(2, "Cannot cd %s\n", buf+3);
+                continue; // repeat while loop so it makes a permant change to the process
+            }
+        }
+        // if not cd cmd 
+        if (fork1() == 0){ // fork returns 0 for child process and PID for parent process
+            runcmd(parsecmd(buf)); // parent forks child and runs runcmd. runcmd terminates itself and dosnt return any value
+        }
+        wait(0); // parent process immediatly begins waiting for child process to finish
+    }
+    exit(0);
+}
+
+//helper funcs
+void panic(char *s){
+    printf(2, "%s\n", s); // prints error msg on the std error given that std error will be file descriptor num 2
+    exit(1); // system call exit, terminates program (1 is error, 0 is a success)
+}
+
+int fork1(void){
+    int pid; // process ID
+    pid = fork();
+    if (pid == -1){
+        panic("fork"); // terminates process if fork had any error
+    }
+    return pid;
+}
 
 // constructor functions 
 
@@ -119,157 +268,7 @@ struct cmd* backcmd(struct cmd *subcmd)
     return (struct cmd*) cmd; // return pointer to the node (typecasted)
 };
 
-
-//helper funcs
-void panic(char *s){
-    fprintf(2, "%s\n", s); // prints error msg on the std error given that std error will be file descriptor num 2
-    exit(1); // system call exit, terminates program (1 is error, 0 is a success)
-}
-
-int fork1(void){
-    int pid; // process ID
-    pid = fork();
-    if (pid == -1){
-        panic("fork"); // terminates process if fork had any error
-    }
-    return pid;
-}
-
-// functions that do things and run cmds
-int fork1(void);
-void panic(char*);
-struct cmd *parsecmd(char*);
-
-struct cmd* nulterminate(struct cmd *cmd) // recursively walks the tree
-{
-    int i;
-    struct backcmd *bcmd;
-    struct execcmd *ecmd;
-    struct listcmd *lcmd;
-    struct pipecmd *pcmd;
-    struct redircmd *rcmd;
-
-    if (cmd == 0){
-        return 0;
-    }
-
-    switch (cmd->type)
-    {
-    case EXEC:
-        ecmd = (struct execcmd*)cmd;
-        // go through argv array in order  and follow the pointer
-        for (i =0; ecmd->argv[i]; i++){
-            // write 0 at eargv
-            *ecmd->eargv[i] =0;
-        }
-        break;
-    case REDIR:
-        rcmd = (struct redircmd*)cmd;
-        nulterminate(rcmd->cmd);
-        *rcmd->efile = 0;
-        break;
-    case PIPE:
-        pcmd = (struct pipecmd*)cmd;
-        nulterminate(pcmd->left);
-        nulterminate(pcmd->right);
-        *rcmd->efile = 0;
-        break;
-    case LIST:
-        lcmd = (struct listcmd*)cmd;
-        nulterminate(lcmd->left);
-        nulterminate(lcmd->right);
-        break;
-    case BACK:
-        bcmd = (struct backcmd*)cmd;
-        nulterminate(bcmd->cmd);
-        break;
-    }
-    return cmd;
-
-}
-
-
-void runcmd(struct cmd *cmd){ // dosnt return jsut finishes by terminating a process
-    int p[2];
-    struct backcmd *bcmd;
-    struct execcmd *ecmd;
-    struct listcmd *lcmd;
-    struct pipecmd *pcmd;
-    struct redircmd *rcmd;
-
-    if (cmd == 0){
-        exit(1);
-    }
-
-    switch (cmd -> type)
-    {
-    default:
-        panic("runcmd");
-
-    case EXEC:
-        ecmd = (struct execcmd*)cmd;
-        // checking to see there is at least one argument
-        if (ecmd->argv[0] == 0){ // argv[0] points to null terminated string of file name of program to execute
-            exit(1);
-        }
-        exec(ecmd->argv[0], ecmd->argv); // sys call exec(filename of program to be run, pointer to array)
-        fprintf(2, "exec %s failed\n", ecmd->argv[0]);
-        break;
-
-    case REDIR:
-        rcmd = (struct redircmd*)cmd;
-        close(rcmd->fd);
-        if (open(rcmd->file, rcmd->mode) < 0){
-            fprintf(2, "open %s failed\n", rcmd->file);
-            exit(1);
-        }
-        runcmd(rcmd->cmd);
-        break;
-
-    case PIPE:
-        pcmd = (struct pipecmd*)cmd;
-        if (pipe(p) < 0){ // system call pipe
-            panic("pipe");
-        }
-        // redirect pgm1 output to pgm2 input (pgm1 | pgm2)
-        if (fork1() == 0){ // creates first child process
-            close(1); //close std out
-            dup(p[1]); // creates copy of fd
-            close(p[0]); // close fd #
-            close(p[1]); // close next fd
-            runcmd(pcmd->left);
-        }
-        if (fork1() == 0){ // creates second child process
-            close(0); // close std in
-            dup(p[0]); // creats copy of file descriptor fd
-            close(p[0]);
-            close(p[1]);
-            runcmd(pcmd->right);
-        }
-        close(p[0]); // close fd 1
-        close(p[1]); // close fd 2
-        wait(0); // wait for first child to terminate
-        wait(0); // waits for other child to terminate
-        break;
-
-    case LIST:
-        lcmd = (struct listcmd*)cmd;
-        if (fork1() == 0){
-            runcmd(lcmd -> left);
-        }
-        wait(0);
-        runcmd(lcmd->right);
-        break;
-
-    case BACK:
-        bcmd = (struct backcmd*)cmd;
-        if (fork1() == 0){
-            runcmd(bcmd->cmd);
-        }
-        break;
-    }
-    exit(0);
-}
+// parsing stuff
 
 char whitespace[] = " \t\r\n\v";
 char symbols[] = "<|>&;()"; // tokens
@@ -319,7 +318,7 @@ int gettoken(char **ps, char *es, char **q, char **eq){
         break;
     default:
         ret = 'a';
-        while (s < es && !strchr(whitespace, *s) && !strchr(symbols))
+        while (s < es && !strchr(whitespace, *s) && !strchr(symbols, *s))
         {
             s++;
         }
@@ -350,6 +349,12 @@ int peek(char **ps, char *es, char*toks){
     return *s && strchr(toks, *s);
 }
 
+struct cmd *parseline(char**, char*);
+struct cmd *parsepipe(char**, char*);
+struct cmd *parseexec(char**, char*);
+struct cmd *nulterminate(struct cmd*);
+
+
 // returns pointer to tree that is built
 struct cmd* parsecmd(char *s){ // pass pointer to input buffer
     char *es;
@@ -360,7 +365,7 @@ struct cmd* parsecmd(char *s){ // pass pointer to input buffer
     peek(&s, es, "");// advance past any white space and rest at null pointer
     // make sure nothing is left
     if (s != es){ // stopped on something besides the null point (end of string)
-        fprintf(2, "leftovers: %s\n", s);
+        printf(2, "leftovers: %s\n", s);
         panic("syntax"); // terminates process, something is wrong
     }
     nulterminate(cmd);
@@ -402,10 +407,13 @@ struct cmd* parseexec(char **ps, char *es){
         // we do the last redir first, then the next until we get to exec
         ret = parseredirs(ret, ps, es); // loop till we see one of the while loop symbols
     }
+    cmd->argv[argc] = 0;
+    cmd->eargv[argc] = 0;
+    return ret;
 }
 
 // gonna add a root and replace it
-struct cmd* parseredirs(struct cmd *cmd, char **ps, char*es){ // pointer to sub tree were going to use
+struct cmd* parseredirs(struct cmd *cmd, char **ps, char *es){ // pointer to sub tree were going to use
     int tok;
     char *q, *eq;
 
@@ -422,13 +430,10 @@ struct cmd* parseredirs(struct cmd *cmd, char **ps, char*es){ // pointer to sub 
             cmd = redircmd(cmd, q, eq, O_RDONLY, 0);
             break;
         case '>':
-            cmd = redircmd(cmd, q, eq, O_WRONLY|O_CREAT|O_TRUNC, 1);
+            cmd = redircmd(cmd, q, eq, O_WRONLY|O_CREAT, 1);
             break;
         case '+':
             cmd = redircmd(cmd, q, eq, O_WRONLY|O_CREAT, 1);
-            break;
-        
-        default:
             break;
         }
     }
@@ -486,38 +491,48 @@ struct cmd* parsepipe(char **ps, char *es){ //pointers tell us where we should l
     return cmd; // return the pipe node
 }
 
+struct cmd* nulterminate(struct cmd *cmd) // recursively walks the tree
+{
+    int i;
+    struct backcmd *bcmd;
+    struct execcmd *ecmd;
+    struct listcmd *lcmd;
+    struct pipecmd *pcmd;
+    struct redircmd *rcmd;
 
-
-
-int main(void){
-    static char buf[100]; // num of chars the buffer will allow (how big of a cmd we can pass)
-    int fd; // file descriptor 
-
-    while ((fd = open("console", O_RDWR)) >= 0) // open for reading and writing
-    {
-        if (fd >= 3){
-            close(fd);
-            break;
-        }
+    if (cmd == 0){
+        return 0;
     }
 
-    // read then run cmd
-    while (getcmd(buf, sizeof(buf)) >= 0)
+    switch (cmd->type)
     {
-        if (buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' ') // checking for cd dir
-        {
-            buf[strlen(buf)-1] = 0; // buf[length of string -1] set to 0
-            if (chdir(buf+3) < 3){ // inokes chdir sys call and points to file name
-                fprintf(2, "Cannot cd %s\n", buf+3);
-                continue; // repeat while loop so it makes a permant change to the process
-            }
+    case EXEC:
+        ecmd = (struct execcmd*)cmd;
+        // go through argv array in order  and follow the pointer
+        for (i =0; ecmd->argv[i]; i++){
+            // write 0 at eargv
+            *ecmd->eargv[i] =0;
         }
-        // if not cd cmd 
-        if (fork1() == 0){ // fork returns 0 for child process and PID for parent process
-            runcmd(parsecmd(buf)); // parent forks child and runs runcmd. runcmd terminates itself and dosnt return any value
-        }
-        wait(0); // parent process immediatly begins waiting for child process to finish
+        break;
+    case REDIR:
+        rcmd = (struct redircmd*)cmd;
+        nulterminate(rcmd->cmd);
+        *rcmd->efile = 0;
+        break;
+    case PIPE:
+        pcmd = (struct pipecmd*)cmd;
+        nulterminate(pcmd->left);
+        nulterminate(pcmd->right);
+        break;
+    case LIST:
+        lcmd = (struct listcmd*)cmd;
+        nulterminate(lcmd->left);
+        nulterminate(lcmd->right);
+        break;
+    case BACK:
+        bcmd = (struct backcmd*)cmd;
+        nulterminate(bcmd->cmd);
+        break;
     }
-    exit(0);
+    return cmd;
 }
-
